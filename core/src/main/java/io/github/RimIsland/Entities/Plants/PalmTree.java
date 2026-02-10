1b15915374683eab6c76e591a81bdb508ff9e07f
CarsenGafford2 <carsengafford2@gmail.com>
2026-02-10 11:42:12 -0700

    Add PalmTree entity class
    Introduce PalmTree class in io.github.RimIsland.Entities.Plants extending Entity. Provides a constructor that accepts a Vector2 position and an empty decideNextTask override as a placeholder for future plant behavior.

---
+++ core/src/main/java/io/github/RimIsland/Entities/Plants/PalmTree.java
@@ -0,0 +1,16 @@
+package io.github.RimIsland.Entities.Plants;
+
+import com.badlogic.gdx.math.Vector2;
+import io.github.RimIsland.Entities.Entity;
+
+public class PalmTree extends Entity {
+
+    public PalmTree(Vector2 position) {
+        super(position);
+    }
+
+    @Override
+    protected void decideNextTask() {
+        
+    }
+}
